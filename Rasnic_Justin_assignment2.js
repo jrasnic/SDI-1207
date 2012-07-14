d860910ca04e39950a6d359e83eea7da9c4cf87c
Justin Rasnic <jrasnic@fullsail.edu>
2012-07-13 21:00:08 -0400

    procedure added

--- Rasnic_Justin_assignment2.js
+++ Rasnic_Justin_assignment2.js
@@ -1,1 +1,17 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+var name = "Arne",
+	daysTravelled = 7,
+	weapons = ["greatsword", "dagger", "bow", "axe"]
+
+// procedure
+var areWeaponsClean = function (clean) {
+	if (clean == "yes") {
+		console.log("Weapons are clean and ready for battle.")
+	} else {
+		console.log("Weapons are dirty. They should be cleaned and readied for battle.")
+	}
+
+}
+
+areWeaponsClean("yes");
