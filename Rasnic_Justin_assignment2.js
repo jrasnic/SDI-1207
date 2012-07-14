618fe5489ab1c2991f86f7ab418765a70809ee01
Justin Rasnic <jrasnic@fullsail.edu>
2012-07-13 21:24:38 -0400

    added boolean function

--- Rasnic_Justin_assignment2.js
+++ Rasnic_Justin_assignment2.js
@@ -2,16 +2,25 @@
 
 var name = "Arne",
 	daysTravelled = 7,
-	weapons = ["greatsword", "dagger", "bow", "axe"]
+	weapons = ["greatsword", "dagger", "bow", "axe"];
 
 // procedure
 var areWeaponsClean = function (clean) {
 	if (clean == "yes") {
-		console.log("Weapons are clean and ready for battle.")
+		console.log("Weapons are clean and ready for battle.");
 	} else {
-		console.log("Weapons are dirty. They should be cleaned and readied for battle.")
-	}
+		console.log("Weapons are dirty. They should be cleaned and readied for battle.");
+	};
+};
 
-}
+// boolean function
+var enterCombat = function (ableToFight, numberOfEnemies) {
+	if (ableToFight >= numberOfEnemies) {
+		console.log("There are only " + numberOfEnemies + " enemies; " + name + " slaughters them!");
+	} else {
+		console.log("There are too many! Run!");
+	};
+};
 
 areWeaponsClean("yes");
+enterCombat(5,4);
