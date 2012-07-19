34f4b5a1e064510a7de4d8a2401f6b4376b6181d
Justin Rasnic <jrasnic@fullsail.edu>
2012-07-19 19:38:37 -0400

    added json file and json data handler to main js file

---
+++ sdi-project3/json.js
@@ -0,0 +1,19 @@
+//JSON Data
+
+var json = {
+	"weapons": {
+		"geatsword": {
+			"name": "Anglachris",
+			"range": 7
+		},
+		"dagger": {
+			"name": "Mithrend",
+			"range": 1
+		},
+		"bow": {
+			"name": "Cumaeth",
+			"range": 40
+		}
+	}
+};
+
--- sdi-project3/rasnic_justin_assignment3.js
+++ sdi-project3/rasnic_justin_assignment3.js
@@ -1,1 +1,12 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+// handle json data
+
+var handleData = function(json){
+	for (var key in json.weapons){
+		var weapon = json.weapons[key];
+		console.log(" weapon : " + key + ", name : " + weapon.name + ", range : " + weapon.range);
+	};
+};
+
+handleData(json);
