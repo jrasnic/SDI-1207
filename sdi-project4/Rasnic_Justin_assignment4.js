ce735c1aec8c81694f8d9b74675ff45d00c17504
Justin Rasnic <jrasnic@fullsail.edu>
2012-07-26 16:00:16 -0400

    first function complete

--- sdi-project4/Rasnic_Justin_assignment4.js
+++ sdi-project4/Rasnic_Justin_assignment4.js
@@ -1,1 +1,38 @@
-alert("JavaScript works!");
+// Justin Rasnic
+// SDI 1207
+// Project 4
+// Library
+
+
+var myLibrary = function () {
+
+	// Does a string follow a 123-456-7890 pattern like a phone number?
+
+	var validPhoneNumCheck = function (phoneNum) {
+		if (phoneNum.length == 12) {
+			if ((phoneNum.charAt(3) == "-") && (phoneNum.charAt(7) == "-")) {             // Make-up: Assignment 2: Boolean Logic
+				phoneNum = phoneNum.slice(0,3) + phoneNum.slice(4,7) + phoneNum.slice(8,12);	
+			}; 
+		};
+		if (phoneNum.length == 10) {
+			var phoneCheck = /\d{10}/;
+			return phoneCheck.test(phoneNum);			
+		} else {return false};
+	};
+	
+
+	// Does a string follow an aaa@bbb.ccc pattern like an email address?
+
+	var
+
+
+
+	return {
+		"validPhoneNumCheck": validPhoneNumCheck
+	};
+
+};
+
+var myLib = myLibrary ();
+
+console.log(myLib.validPhoneNumCheck("203-297-5077"));
